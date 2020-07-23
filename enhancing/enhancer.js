53067410d57c0d9261fb7a2f7eb696ef06c92fe9
unknown <prakashbahadursingh2@gmail.com>
2020-07-23 08:53:50 -0700

    repair success, and fail testing complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,31 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement < 20){
+    item.enhancement = item.enhancement +1
+  }  else{
+    item.enhancement = item.enhancement
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if(item.enhancement < 15){
+    item.durability = item.durability -5
+  }else if
+    (item.enhancement > 16){
+    item.durability = item.durability -10
+  }
+   if(item.enhancement < 16){
+    item.enhancement = item.enhancement -1
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100
   return { ...item };
 }
-
 function get(item) {
   return { ...item };
 }
